f76aba7c5c7501f236f35a0f2e6eab302d599885
Edvard <mejik-yo@mail.ru>
2019-10-12 02:23:24 +0300

    test

--- src/app/components/board/card-list/card-list.component.ts
+++ src/app/components/board/card-list/card-list.component.ts
@@ -4,7 +4,7 @@ import { ICardList } from '../../../models/cards'
 @Component({
   selector: 'app-card-list',
   templateUrl: './card-list.component.html',
-  styleUrls: ['./card-list.component.scss']
+  styleUrls: ['./card-list.component.scss'],
 })
 export class CardListComponent implements OnInit {
   @Input() cardList: ICardList;
